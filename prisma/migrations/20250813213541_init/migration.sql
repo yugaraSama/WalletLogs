261c2ae256902f199253a0eea3fb0d67b937311f
yugara <francois@woje.com>
2025-08-13 23:38:07 +0200

    getting back some mistakes

--- prisma/migrations/20250813213541_init/migration.sql
+++ prisma/migrations/20250813213541_init/migration.sql
@@ -32,6 +32,7 @@ CREATE TABLE `Income` (
     `recurence` VARCHAR(191) NULL DEFAULT 'monthly',
     `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
 
+    INDEX `Income_userId_fkey`(`userId`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
@@ -43,6 +44,7 @@ CREATE TABLE `Transaction` (
     `type` VARCHAR(191) NOT NULL,
     `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
 
+    INDEX `Transaction_userId_fkey`(`userId`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
